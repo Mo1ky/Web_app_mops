030edaf0626302a425ca2e54b64d80a6a4b26094
Mo1ky <119064533+Mo1ky@users.noreply.github.com>
2024-03-13 22:17:45 +0300

    Delete routes directory

--- routes/users.route.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { ensureLoggedOut, ensureLoggedIn } = require('connect-ensure-login');
-
-router.get('/', ensureLoggedIn({ redirectTo: '/auth/login' }), (req, res, next) => {
-  
-  res.render('users');
-});
-
-module.exports = router;
